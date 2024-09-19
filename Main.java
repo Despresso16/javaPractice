d11cdcf1b247babd84b23f2d19cc9c60f41b368a
Despresso <118217896+Despresso16@users.noreply.github.com>
2024-09-19 19:32:53 +0200

    final update
    finished

--- Main.java
+++ Main.java
@@ -1,7 +1,4 @@
-import java.util.ArrayList;
-import java.util.Objects;
-import java.util.Scanner;
-import java.util.List;
+import java.util.*;
 
 
 import static java.lang.Math.decrementExact;
@@ -12,195 +9,115 @@ import static java.lang.Math.pow;
 public class Main {
     public static void main(String[] args) {
 
-        //zad z moodle
-//        //zad1
-//        System.out.println("Zad1");
-//        System.out.printf("Podaj liczbe: ");
-//        Scanner scanner = new Scanner(System.in);
-//        String input = scanner.nextLine();
-//        float fNum = Float.valueOf(input);
-//        System.out.println("Liczba: " + fNum);
-//        //zad2
-//        System.out.println("Zad2");
-//        int a = 25, b = 7;
-//        System.out.println("Dodawanie: " + (a+b));
-//        System.out.println("Odejmowanie: " + (a-b));
-//        System.out.println("Mnozenie: " + (a*b));
-//        System.out.println("Dzielenie: " + (a/b));
-//        System.out.println("Modulo: " + (a%b));
-//        //zad3
-//        System.out.println("Zad3");
-//        System.out.printf("Podaj liczbe: ");
-//        input = scanner.nextLine();
-//        int num = Integer.valueOf(input);
-//        if(num % 2 == 0){
-//            System.out.println("Ta liczba jest parzysta");
-//        }
-//        else System.out.println("Ta liczba jest nieparzysta");
-//        //zad4
-//        System.out.println("Zad4");
-//        System.out.printf("Podaj ocene: ");
-//        input = scanner.nextLine();
-//        num = Integer.valueOf(input);
-//        switch (num){
-//            case 1:
-//                System.out.println("Ocena niedostateczna");
-//                break;
-//            case 2:
-//                System.out.println("Ocena dopuszczajaca");
-//                break;
-//            case 3:
-//                System.out.println("Ocena dostateczny");
-//                break;
-//            case 4:
-//                System.out.println("Ocena dobry");
-//                break;
-//            case 5:
-//                System.out.println("Ocena bardzo dobry");
-//                break;
-//            case 6:
-//                System.out.println("Ocena celujaca");
-//                break;
-//            default:
-//                System.out.println("Nie ma takiej oceny!");
-//                break;
-//        }
-//        //zad5
-//        System.out.println("Zad5");
-//        System.out.printf("Podaj liczbe ocen: ");
-//        int arrayLength = Integer.valueOf(scanner.nextLine());
-//        int array[] = new int[arrayLength];
-//        float fresult = 0;
-//        for(int i = 0; i < arrayLength; i++){
-//            array[i] = Integer.valueOf(scanner.nextLine());
-//            if(array[i] < 1) array[i] = 1;
-//            if(array[i] > 6) array[i] = 6;
-//            fresult += array[i];
-//        }
-//        fresult /= arrayLength;
-//        System.out.println("Srednia ocen: " + fresult);
-//        //zad6
-//        System.out.println("Zad6");
-//        System.out.printf("Podaj n: ");
-//        int n = Integer.valueOf(scanner.nextLine());
-//        int result = 1;
-//        for(int i = 2; i <= n; i++){
-//            result *= i;
-//        }
-//        System.out.println("Silnia: " + result);
-//        //zad7
-//        System.out.println("Zad7");
-//        for(int i = 100; i > 0; i -= 2){
-//            System.out.printf(i + " ");
-//        }
 
         //pdf nr1
-//        //zad1
-//        System.out.println("Zad1");
+        //zad1
+        System.out.println("Zad1");
         Scanner sc = new Scanner(System.in);
-//        System.out.print("Podaj temp. w st. C: ");
-//        Float celsius = Float.valueOf(sc.nextLine());
-//        System.out.println("Temperatura w st. F: " + (1.8 * celsius + 32));
-//        //zad2
-//        System.out.println("Zad2");
-//        System.out.print("Podaj 1 liczbe: ");
-//        int a = Integer.valueOf(sc.nextLine());
-//        System.out.print("Podaj 2 liczbe: ");
-//        int b = Integer.valueOf(sc.nextLine());
-//        System.out.print("Podaj 3 liczbe: ");
-//        int c = Integer.valueOf(sc.nextLine());
-//        int max = a;
-//        if(max < b) max = b;
-//        if(max < c) max = c;
-//        System.out.println("Najwieksza liczba: " + max);
-//        //zad3
-//        System.out.println("Zad3");
-//        System.out.print("Podaj wage w kg: ");
-//        double waga = sc.nextDouble();
-//        System.out.print("Podaj wzrost w metrach: ");
-//        double wzrost = sc.nextDouble();
-//        Double bmi = waga / Math.pow(wzrost, 2);
-//        System.out.println("BMI: " + String.format("%.2f", bmi));
-//        //zad4
-//        System.out.println("Zad4");
-//        System.out.print("Podaj dochod: ");
-//        double dochod = sc.nextDouble();
-//        double podatek = 0;
-//        if(dochod < 85528){
-//            podatek = (dochod  - 556.02) * 0.18;
-//        }
-//        else podatek = 14839.02 + ((dochod - 85528) * 0.32);
-//        System.out.print("Twoj podatek wynosi: " + String.format("%.2f", podatek));
-//        //zad5
-//        System.out.println("Zad5");
-//        System.out.print("Podaj cene (od 100zl do 10000zl): ");
-//        double cena = sc.nextDouble();
-//        if(cena > 10000) cena = 10000;
-//        else if(cena < 100) cena = 100;
-//        System.out.print("Podaj ilosc rat (od 6 do 48): ");
-//        int liczbaRat = sc.nextInt();
-//        if(liczbaRat > 48) liczbaRat = 48;
-//        else if(liczbaRat < 6) liczbaRat = 6;
-//        double procent = 0.0;
-//        if(liczbaRat < 12) procent = 2.5;
-//        else if(liczbaRat > 12 && liczbaRat <24) procent = 5;
-//        else procent = 10;
-//        double rata = (cena / liczbaRat) * (1.0 + (procent / 100));
-//        double odsetki = (cena / liczbaRat) * (1.0 + (procent / 100)) * 31/365;
-//        System.out.println("Miesieczna rata: " + String.format("%.2f", rata) + "| odsetki: " + String.format("%.2f", odsetki));
-//        //zad 6
-//        String endInput = "reset";
-//        float result = 0;
-//        boolean cont = false;
-//        int num1 = 0;
-//        String operator = "";
-//        System.out.println("Zad6");
-//        while(!Objects.equals(endInput, "")){
-//            if(cont) {
-//                num1 = (int) result;
-//            }
-//            else{
-//                num1 = sc.nextInt();
-//            }
-//            operator = sc.next();
-//            if(!Objects.equals(operator, "+") && !Objects.equals(operator, "-") && !Objects.equals(operator, "*") && !Objects.equals(operator, "/")){
-//                System.out.println(operator);
-//                System.out.println("Taki operator nie istnieje");
-//                continue;
-//            }
-//            int num2 = sc.nextInt();
-//            if(operator.equals("+")){
-//                result = num1 + num2;
-//            }
-//            else if(operator.equals("-")){
-//                result = num1 - num2;
-//            }
-//            else if(operator.equals("*")){
-//                result = num1 * num2;
-//            }
-//            else if(operator.equals("/")){
-//                result = num1 / num2;
-//            }
-//            System.out.println("= " + String.format("%.2f", result));
-//            System.out.println("Zakoncz- wpisz end | Koontynuuj - wpisz cont | Zresetuj - wpisz reset: ");
-//            endInput = sc.next();
-//            if(Objects.equals(endInput, "end")){
-//                break;
-//            }
-//            else if(Objects.equals(endInput, "reset")){
-//                System.out.println("Zresetowano!");
-//            }
-//            else {
-//                cont = true;
-//                System.out.println((int) result);
-//            }
-//        }
+        int num = 0;
+        System.out.print("Podaj temp. w st. C: ");
+        Float celsius = Float.valueOf(sc.nextLine());
+        System.out.println("Temperatura w st. F: " + (1.8 * celsius + 32));
+        //zad2
+        System.out.println("Zad2");
+        System.out.print("Podaj 1 liczbe: ");
+        int a = Integer.valueOf(sc.nextLine());
+        System.out.print("Podaj 2 liczbe: ");
+        int b = Integer.valueOf(sc.nextLine());
+        System.out.print("Podaj 3 liczbe: ");
+        int c = Integer.valueOf(sc.nextLine());
+        int max = a;
+        if(max < b) max = b;
+        if(max < c) max = c;
+        System.out.println("Najwieksza liczba: " + max);
+        //zad3
+        System.out.println("Zad3");
+        System.out.print("Podaj wage w kg: ");
+        double waga = sc.nextDouble();
+        System.out.print("Podaj wzrost w metrach: ");
+        double wzrost = sc.nextDouble();
+        Double bmi = waga / Math.pow(wzrost, 2);
+        System.out.println("BMI: " + String.format("%.2f", bmi));
+        //zad4
+        System.out.println("Zad4");
+        System.out.print("Podaj dochod: ");
+        double dochod = sc.nextDouble();
+        double podatek = 0;
+        if(dochod < 85528){
+            podatek = (dochod  - 556.02) * 0.18;
+        }
+        else podatek = 14839.02 + ((dochod - 85528) * 0.32);
+        System.out.print("Twoj podatek wynosi: " + String.format("%.2f", podatek));
+        //zad5
+        System.out.println("Zad5");
+        System.out.print("Podaj cene (od 100zl do 10000zl): ");
+        double cena = sc.nextDouble();
+        if(cena > 10000) cena = 10000;
+        else if(cena < 100) cena = 100;
+        System.out.print("Podaj ilosc rat (od 6 do 48): ");
+        int liczbaRat = sc.nextInt();
+        if(liczbaRat > 48) liczbaRat = 48;
+        else if(liczbaRat < 6) liczbaRat = 6;
+        double procent = 0.0;
+        if(liczbaRat < 12) procent = 2.5;
+        else if(liczbaRat > 12 && liczbaRat <24) procent = 5;
+        else procent = 10;
+        double rata = (cena / liczbaRat) * (1.0 + (procent / 100));
+        double odsetki = (cena / liczbaRat) * (1.0 + (procent / 100)) * 31/365;
+        System.out.println("Miesieczna rata: " + String.format("%.2f", rata) + "| odsetki: " + String.format("%.2f", odsetki));
+        //zad 6
+        String endInput = "reset";
+        float result = 0;
+        boolean cont = false;
+        int num1 = 0;
+        String operator = "";
+        System.out.println("Zad6");
+        while(!Objects.equals(endInput, "")){
+            if(cont) {
+                num1 = (int) result;
+            }
+            else{
+                num1 = sc.nextInt();
+            }
+            operator = sc.next();
+            if(!Objects.equals(operator, "+") && !Objects.equals(operator, "-") && !Objects.equals(operator, "*") && !Objects.equals(operator, "/")){
+                System.out.println(operator);
+                System.out.println("Taki operator nie istnieje");
+                continue;
+            }
+            int num2 = sc.nextInt();
+            if(operator.equals("+")){
+                result = num1 + num2;
+            }
+            else if(operator.equals("-")){
+                result = num1 - num2;
+            }
+            else if(operator.equals("*")){
+                result = num1 * num2;
+            }
+            else if(operator.equals("/")){
+                result = num1 / num2;
+            }
+            System.out.println("= " + String.format("%.2f", result));
+            System.out.println("Zakoncz- wpisz end | Koontynuuj - wpisz cont | Zresetuj - wpisz reset: ");
+            endInput = sc.next();
+            if(Objects.equals(endInput, "end")){
+                break;
+            }
+            else if(Objects.equals(endInput, "reset")){
+                System.out.println("Zresetowano!");
+            }
+            else {
+                cont = true;
+                System.out.println((int) result);
+            }
+        }
         //pdf nr 2
 
         //zad 1
         System.out.println("Zad1");
         System.out.print("Podaj liczbe dodatnia: ");
-        int num = sc.nextInt();
+        num = sc.nextInt();
         if(num < 0){
             System.out.println("Miales podac dodatnia");
             num *= -1;
@@ -257,7 +174,8 @@ public class Main {
                 numList.add(num);
             }
         }
-        int min= numList.get(0), max = numList.get(0);
+        int min= numList.get(0);
+        max = numList.get(0);
         for(int i = 1; i < numList.size(); i++){
             if(min > numList.get(i)) min = numList.get(i);
             if(max < numList.get(i)) max = numList.get(i);
@@ -266,6 +184,108 @@ public class Main {
         System.out.println("Min: " + min + " Max: " + max + " Avg min i max: " + avg);
         //zad 6
         System.out.println("Zad6");
-        //...
+        Random rnd = new Random();
+        int rndNum = rnd.nextInt(100) + 1;
+        while(rndNum != num){
+            System.out.print("Zgadnij liczbe od 1 do 100: ");
+            num = sc.nextInt();
+            if(num > rndNum) System.out.println("Podałeś za dużą wartość");
+            else if (num < rndNum) System.out.println("Podałeś za małą wartość");
+            else System.out.println("Gratulacje!");
+        }
+        //zad 7
+        System.out.println("Zad7");
+        System.out.print("Podaj znak wypelnienia prostokata: ");
+        char fillerChar = sc.next().charAt(0);
+        System.out.println(fillerChar);
+        int x, y;
+        System.out.print("Podaj pozycje x lewego rogu: ");
+        x = sc.nextInt();
+        System.out.print("Podaj pozycje y lewego rogu: ");
+        y = sc.nextInt();
+        System.out.print("Podaj dlugosc boku a: ");
+        a = sc.nextInt();
+        System.out.print("Podaj dlugosc boku b: ");
+        b = sc.nextInt();
+        for (int i = 1; i <= y + b; i++) {
+            for (int j = 1; j <= x + a; j++) {
+                if (i <= y || j <= x) {
+                    System.out.print(' ');
+                } else {
+                    System.out.print(fillerChar);
+                }
+            }
+            System.out.println();
+        }
+        //zad 8
+        System.out.println("Zad8");
+        System.out.print("Podaj wysokosc choinki: ");
+        int n = sc.nextInt();
+
+        if (n <= 0) {
+            System.out.println("Wysokosc choinki musi być wieksza niz 0.");
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        //zad 9
+        System.out.println("Zad9");
+        System.out.print("Podaj liczbę calkowita: ");
+        int number = sc.nextInt();
+        int sumOfDigits = 0;
+        int evenSum = 0, oddSum = 0;
+        int evenCount = 0, oddCount = 0;
+        int tempNumber = Math.abs(number);
+        while (tempNumber > 0) {
+            int digit = tempNumber % 10;
+            sumOfDigits += digit;
+
+            if (digit % 2 == 0) {
+                evenSum += digit;
+                evenCount++;
+            } else {
+                oddSum += digit;
+                oddCount++;
+            }
+
+            tempNumber /= 10;
+        }
+        System.out.println("Suma cyfr: " + sumOfDigits);
+        double evenAvg = evenCount > 0 ? (double) evenSum / evenCount : 0;
+        double oddAvg = oddCount > 0 ? (double) oddSum / oddCount : 0;
+        System.out.println("Średnia parzystych: " + evenCount);
+        System.out.println("Średnia nieparzystych: " + oddCount);
+        if (evenCount > 0 && oddCount > 0) {
+            double ratio = evenAvg / oddAvg;
+            System.out.println("Stosunek średniej cyfr parzystych do nieparzystych: " + String.format("%.2f", ratio));
+        } else {
+            System.out.println("Nie można obliczyć stosunku");
+        }
+        //zad 10
+        System.out.println("Zad10");
+        System.out.print("Podaj liczbe: ");
+        num = sc.nextInt();
+        for(int i = 1; i <= num; i++){
+            if(num % i == 0) System.out.print(i + ", ");
+        }
+        System.out.println();
+        //zad 11
+        System.out.println("Zad11");
+        System.out.print("Podaj liczbe: ");
+        boolean jestPierwsza = true;
+        num = sc.nextInt();
+        for(int i = 2; i < num; i++){
+            if(num % i == 0) jestPierwsza = false;
+        }
+        if(jestPierwsza) System.out.println("Ta liczba jest pierwsza");
+        else System.out.println("Ta liczba nie jest pierwsza");
     }
 }
